c50a337be73de9e67b9d74ebcaa11c2612cfa611
Tony Rawlins <tjrawlins.dev@gmail.com>
2024-02-02 10:27:48 -0700

    updated technologies

--- src/app/technology/technology.component.ts
+++ src/app/technology/technology.component.ts
@@ -3,28 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-technology',
   templateUrl: './technology.component.html',
-  styleUrls: ['./technology.component.css']
+  styleUrls: ['./technology.component.css'],
 })
 export class TechnologyComponent {
-  name: string = "Technologies";
+  name: string = 'Technologies';
   technologies: string[] = [
-    "Git",
-    "Github",
-    "HTML",
-    "CSS",
-    "Javascript",
-    "C#",
-    "EF",
-    "Java",
-    "Hibernate",
-    "Spring",
-    "WebApi",
-    "Bootstrap",
-    "SQL",
-    "MySQL",
-    "Microsoft Azure",
-    "Typescript",
-    "Angular",
-    "Python"
+    'Git',
+    'Github',
+    'HTML',
+    'CSS',
+    'Javascript',
+    'C#',
+    'EF',
+    'Java',
+    'Hibernate',
+    'Spring',
+    'WebApi',
+    'Bootstrap',
+    'Material UI',
+    'Syncfusion',
+    'SQL',
+    'MySQL',
+    'Microsoft Azure',
+    'Typescript',
+    'Angular',
+    'React',
+    'Python',
   ];
 }
